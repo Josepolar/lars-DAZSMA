b100e8c349bf1659737eed9cadee4ccb15044c9a
PolarDredd <josebernardfernandez@gmail.com>
2025-09-18 23:20:39 +0800

    Update background image URL and add cache-busting query string to CSS links

--- students/student-home.php
+++ students/student-home.php
@@ -79,7 +79,7 @@ if ($userStats && $userStats['total_available_activities'] > 0) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Student Dashboard - LARS</title>
-        <link rel="stylesheet" href="student-home.css">
+        <link rel="stylesheet" href="student-home.css?v=<?php echo time(); ?>">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     </head>
     <body>
