5e079d2c6e5d1fe682f4592f08432df3c3df4b19
PolarDredd <josebernardfernandez@gmail.com>
2025-09-18 23:49:54 +0800

    Add cache-busting to admin-userman.css link in admin-userman.php

--- admin/admin-userman.php
+++ admin/admin-userman.php
@@ -106,7 +106,7 @@ $teachers = $result->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin-userman.css">
+    <link rel="stylesheet" href="admin-userman.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         .password-container {
